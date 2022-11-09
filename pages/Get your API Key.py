584d78b2bd113a8eb5d809d94b770d36e70e1e5c
Emad_ASUS <e.a.darwich@gmail.com>
2022-11-09 23:40:52 +0100

    get api full modif.

--- pages/Get your API Key.py
+++ pages/Get your API Key.py
@@ -6,3 +6,7 @@ st.markdown(""" ##### 1. First you have to connect to [Edenai](https://www.edena
 st.markdown(""" ##### 2. Then click on `Sign in` and complete the various fields requested to create an account""")
 st.image('images/edenai_signup.png')
 st.markdown(""" ##### 3. When you have created your account you can login by clicking on `Get Started`, the site will take you to the home page. On the homepage, you will find a tab called `Get your API Key`. Click on it and the site will reveal your API key named `Your API Token`. """)
+st.image('images/edenai_homepage.png')
+st.image('images/edenai_key.png')
+st.markdown(""" ##### 4. All you have to do is copy this key and paste it on the registration form of our site to take advantage of all its features.""")
+st.image('images/singup_enter_your_key.png')
