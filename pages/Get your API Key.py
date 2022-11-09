7189ed0ed4069aa3527f69b77b4764bbda832b92
Emad_ASUS <e.a.darwich@gmail.com>
2022-11-09 23:33:18 +0100

    get api modif.

--- pages/Get your API Key.py
+++ pages/Get your API Key.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 
 st.header("HOW GET YOUR FREE API KEY")
-url = 
 st.markdown(""" ##### 1. First you have to connect to [Edenai](https://www.edenai.co/post/which-sentiment-analysis-api-to-choose-for-your-project)""") 
 st.markdown(""" ##### 2. Then click on `Sign in` and complete the various fields requested to create an account""")
 st.image('images/edenai_signup.png')
+st.markdown(""" #####  """)
