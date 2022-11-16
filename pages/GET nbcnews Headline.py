7d0eadbf52a0efbbe46b5bb74190a08a116a811c
Emad_ASUS <e.a.darwich@gmail.com>
2022-11-16 21:59:09 +0100

    add new page ncbNews

---
+++ pages/GET nbcnews Headline.py
@@ -0,0 +1,12 @@
+import requests
+from bs4 import BeautifulSoup
+import streamlit as st
+
+
+url = "https://www.nbcnews.com/"
+response = requests.get(url)
+html = BeautifulSoup(response.text, "html.parser")
+for news in html.find_all("h3")[4]:
+    st.header(f"Headline: {news.text}")
+    st.markdown("___")
+    st.subheader(f"Link:  {news.find('a')['href']}")
