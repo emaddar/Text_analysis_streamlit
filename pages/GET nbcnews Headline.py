918606fc59aa56bd23b1697bbdf26478ac462a30
Emad_ASUS <e.a.darwich@gmail.com>
2022-11-16 22:17:12 +0100

    headlines

--- pages/GET nbcnews Headline.py
+++ pages/GET nbcnews Headline.py
@@ -4,7 +4,9 @@ import streamlit as st
 url = "https://www.nbcnews.com/"
 response = requests.get(url)
 html = BeautifulSoup(response.text, "html.parser")
-st.header("test")
-for news in html.find_all("h3")[:4]:
-    st.header(f"Headline: {news.text}")
-    st.subheader(f"Link:  {news.find('a')['href']}")
+st.header("NBC news headlines")
+i = 1
+for news in html.find_all("h3"):
+    st.write(f"{i} : [{news.text}]({news.find('a')['href']})")
+    i += 1
+    # st.subheader(f"Link:  {news.find('a')['href']}")
