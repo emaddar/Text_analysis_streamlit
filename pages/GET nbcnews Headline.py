af919181e90cf744d1c6c128df9e0d18fe2edbde
Emad_ASUS <e.a.darwich@gmail.com>
2022-11-17 07:21:56 +0100

    add click here

--- pages/GET nbcnews Headline.py
+++ pages/GET nbcnews Headline.py
@@ -1,14 +1,17 @@
 import requests
 from bs4 import BeautifulSoup
 import streamlit as st
-url = "https://www.nbcnews.com/"
-response = requests.get(url)
-html = BeautifulSoup(response.text, "html.parser")
 
-st.write(html.find_all("h3"))
-st.header("NBC news headlines")
-i = 1
-for news in html.find_all("h3"):
-    st.write(f"{i} : [{news.text}]({news.find('a')['href']})")
-    i += 1
-    # st.subheader(f"Link:  {news.find('a')['href']}")
+click = st.button('click here')
+if click:
+    url = "https://www.nbcnews.com/"
+    response = requests.get(url)
+    html = BeautifulSoup(response.text, "html.parser")
+
+    st.write(html.find_all("h3"))
+    st.header("NBC news headlines")
+    i = 1
+    for news in html.find_all("h3"):
+        st.write(f"{i} : [{news.text}]({news.find('a')['href']})")
+        i += 1
+        # st.subheader(f"Link:  {news.find('a')['href']}")
