4476689ffed28277cc852138bd6ca481a5b55938
Emad_ASUS <e.a.darwich@gmail.com>
2022-11-17 22:21:43 +0100

    Fix buttom

--- pages/GET nbcnews Headline.py
+++ pages/GET nbcnews Headline.py
@@ -2,16 +2,17 @@ import requests
 from bs4 import BeautifulSoup
 import streamlit as st
 
-click = st.button('click here')
+click = st.button('click here to get last news from NBCnews')
 if click:
     url = "https://www.nbcnews.com/"
     response = requests.get(url)
-    st.write(response)
-    html = BeautifulSoup(response.text, "html.parser")
-
-    st.write(html.find_all("h3"))
-    st.header("NBC news headlines")
-    i = 1
-    for news in html.find_all("h3"):
-        st.write(f"{i} : [{news.text}]({news.find('a')['href']})")
-        i += 1
+    if response.status_code == 200:
+        html = BeautifulSoup(response.text, "html.parser")
+        # st.write(html.find_all("h3"))
+        # st.header("NBC news headlines")
+        i = 1
+        for news in html.find_all("h3"):
+            st.write(f"{i} : [{news.text}]({news.find('a')['href']})")
+            i += 1
+    else : 
+        st.write("Sorry, we can't get last Headlines")
