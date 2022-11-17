98e0b4d0ebfc6ab0b3b3e9b070cc28a747c86b51
Emad_ASUS <e.a.darwich@gmail.com>
2022-11-17 07:22:49 +0100

    response

--- pages/GET nbcnews Headline.py
+++ pages/GET nbcnews Headline.py
@@ -6,6 +6,7 @@ click = st.button('click here')
 if click:
     url = "https://www.nbcnews.com/"
     response = requests.get(url)
+    st.write(response)
     html = BeautifulSoup(response.text, "html.parser")
 
     st.write(html.find_all("h3"))
@@ -14,4 +15,3 @@ if click:
     for news in html.find_all("h3"):
         st.write(f"{i} : [{news.text}]({news.find('a')['href']})")
         i += 1
-        # st.subheader(f"Link:  {news.find('a')['href']}")
